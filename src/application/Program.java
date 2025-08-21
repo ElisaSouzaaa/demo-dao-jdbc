fdfce894082a330d342b75c9f5d8c9cd9af2fe3c
Elisa Souza <contato.elisasouza@hotmail.com>
2025-08-21 18:50:46 -0300

    Seller entitie class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,11 +1,16 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obg = new Department(1,"books");
+        Seller seller = new Seller(22,"Bob","bob@gmail.com",new Date(), 3000.0, obg);
 
         System.out.println(obg);
+        System.out.println(seller);
     }
 }
